5116c157d767d57ff40b276bc270e3d3667af588
joelburdorf <cozlikb@gmail.com>
2020-02-25 10:51:26 -0600

    added a reducer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App JS</h1>
-    </div>
-  );
+ 
+class App extends Component{
+
+    handleClick = () => {
+      this.props.dispatch (
+        {
+          type: 'BUTTON_ONE'
+          }
+      )
+    }
+    render(){
+
+    return (
+      <div className="App">
+        <h1>App JS</h1>
+        <button onClick={this.handleClick}>Button One</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeInstance = createStore(
+    () => {
+        console.log("Hello, I'm a reducer");
+        
+    }
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
